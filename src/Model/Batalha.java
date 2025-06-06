4d2c15ca63d0b1f719b6e440342c85b53fc4c384
Lucas Bruno <cucabruno06@gmail.com>
2025-06-06 11:05:27 -0300

    Iniciando as classes de batalha
    Começando os primeiros passos nas classes que controlaram as batalhas

--- src/Model/Batalha.java
+++ src/Model/Batalha.java
@@ -1,4 +1,71 @@
 package Model;
 
 public class Batalha {
+    private Treinador treinador1;
+    private Treinador treinador2;
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+    private String vencedor;
+
+    public Batalha(Treinador trei1, Treinador trei2, Pokemon poke1, Pokemon poke2, String Vencedor){
+        this.treinador1 = trei1;
+        this.treinador2 = trei2;
+        this.pokemon1 = poke1;
+        this.pokemon2 = poke2;
+        this.vencedor = Vencedor;
+    }
+
+    public void batalhar() {
+        this.vencedor = RegrasBatalha.definirVencedor(pokemon1, pokemon2);
+        if (vencedor.equals(treinador1.getNome())) {
+            pokemon1.ganharExperiencia(20);
+        } else {
+            pokemon2.ganharExperiencia(20);
+        }
+    }
+
+    public String getResumo() {
+        return "Resultado da última batalha" + ": " + treinador1.getNome() + " vs " + treinador2.getNome() +
+                " — Vencedor: " + vencedor;
+    }
+
+    public Treinador getTreinador1() {
+        return treinador1;
+    }
+
+    public void setTreinador1(Treinador treinador1) {
+        this.treinador1 = treinador1;
+    }
+
+    public Treinador getTreinador2() {
+        return treinador2;
+    }
+
+    public void setTreinador2(Treinador treinador2) {
+        this.treinador2 = treinador2;
+    }
+
+    public Pokemon getPokemon1() {
+        return pokemon1;
+    }
+
+    public void setPokemon1(Pokemon pokemon1) {
+        this.pokemon1 = pokemon1;
+    }
+
+    public Pokemon getPokemon2() {
+        return pokemon2;
+    }
+
+    public void setPokemon2(Pokemon pokemon2) {
+        this.pokemon2 = pokemon2;
+    }
+
+    public String getVencedor() {
+        return vencedor;
+    }
+
+    public void setVencedor(String vencedor) {
+        this.vencedor = vencedor;
+    }
 }
--- src/Model/HistoricoBatalhas.java
+++ src/Model/HistoricoBatalhas.java
@@ -1,4 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class HistoricoBatalhas {
+    batalhas = new ArrayList<>();
+
+
+    public void adicionar(Batalha b) {
+        batalhas.add(b);
+    }
+
+    public List<Batalha> listar() {
+        return Collections.unmodifiableList(batalhas);
+    }
+
+    public boolean remover(int index) {
+        if (index >= 0 && index < batalhas.size()) {
+            batalhas.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public int total() {
+        return batalhas.size();
+    }
 }
