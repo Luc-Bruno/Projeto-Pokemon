deb14a155a07f1910c942121877356572de342bf
Swiatyi <Swiatyi44@gmail.com>
2025-06-07 16:59:36 -0300

    Criação de um Menuzinho na View Treinador
    Criação de metodos de Adicionar, Listar e Remover

--- src/View/TreinadorView.java
+++ src/View/TreinadorView.java
@@ -1,4 +1,54 @@
 package View;
 
+import Controller.TreinadorController;
+import Model.Treinador;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TreinadorView {
+    private Scanner scan = new Scanner(System.in);
+    private TreinadorController controleT = new TreinadorController();
+
+    public void menuT(){
+        int op;
+        do{
+            System.out.println("---MENU TREINADOR---");
+            System.out.println("(1). Cadastrar Treinador");
+            System.out.println("(2). Listar Treinadores");
+            System.out.println("(3). Remover Treinador");
+            System.out.println("(0). Sair");
+            op = scan.nextInt();scan.nextLine();
+            switch(op){
+                case 1 -> cadastrarT();
+                case 2 -> listarT();
+                case 3 -> removerT();
+                case 0 -> System.out.println("Saindo...");
+                default -> System.out.println("Opção inválida!\nEscolha uma Opção Válida!!");
+            }
+        }while(op != 0);
+    }
+
+    public void cadastrarT(){
+        System.out.print("Nome: ");
+        String nome = scan.nextLine();
+        System.out.print("Idade: ");
+        int idade = scan.nextInt();scan.nextLine();
+        controleT.cadastrarTreinador(nome, idade);
+    }
+
+    public void listarT(){
+        ArrayList<Treinador> treinadores = controleT.listarTreinadores();
+        for (Treinador t : treinadores){
+            System.out.println("Nome: " + t.getNome() + ", Idade: " + t.getIdade());
+        }
+    }
+
+    public void removerT(){
+        System.out.println("Nome do Treinador que deseja remover: ");
+        String nome = scan.nextLine();
+        controleT.removerTreinador(nome);
+    }
+
+
 }
