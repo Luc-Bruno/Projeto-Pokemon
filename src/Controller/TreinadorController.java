e5cd94ee7620e791e4320c38f64474760b3ca094
Swiatyi <Swiatyi44@gmail.com>
2025-06-09 15:34:06 -0300

    Atualização do Controller e View do Treinador
    Atualizei o View com try e catch pra ler o Nome e a Idade do Treinador e adicionei o metodo atualizar, para mudar as informações do Treinador, coloquei tambem mudanças futuras em comentarios para salvar o cadastro, remoção e atualização no arquivo

--- src/Controller/TreinadorController.java
+++ src/Controller/TreinadorController.java
@@ -8,9 +8,17 @@ import java.util.ArrayList;
 public class TreinadorController {
     private TreinadorRepository repositorioT = new TreinadorRepository();
 
+    /*public TreinadorController(){
+        ArrayList<Treinador> treinadores = ArquivTreinador.carregar();
+        for(Treinador t : treinadores){
+            repositorioT.adcionarTrein(t);
+        }
+    }
+*/
     public void cadastrarTreinador(String nome, int idade){
         Treinador t = new Treinador(nome, idade);
         repositorioT.adcionarTrein(t);
+        //ArquivoTreinador.salvar(repository.listar());
         System.out.println("Treinador cadastrado com sucesso!!");
     }
 
@@ -21,6 +29,7 @@ public class TreinadorController {
     public void removerTreinador(String nome){
         boolean sucesso = repositorioT.removerTreinador(nome);
         if (sucesso){
+            //ArquivoTreinador.salvar(repository.listar());
             System.out.println("Treinador removido!!");
         }else{
             System.out.println("Treinador não encontrado!!");
@@ -31,5 +40,16 @@ public class TreinadorController {
         return repositorioT.buscarPorNome(nome);
     }
 
+    public void atualizarTreinador(String nomeAntigo, String novoNome, int novaIdade){
+        Treinador treinador = repositorioT.buscarPorNome(nomeAntigo);
+        if(treinador != null){
+            treinador.setNome(novoNome);
+            treinador.setIdade(novaIdade);
+            //ArquivoTreinador.salvar(repositorioT.listarTreinador()); SALVAR NO REPOSITORIO
+            System.out.println("Treinador atualizado com sucesso!");
+        }else{
+            System.out.println("Treinador não encontrado !!");
+        }
 
+    }
 }
--- src/View/TreinadorView.java
+++ src/View/TreinadorView.java
@@ -17,12 +17,14 @@ public class TreinadorView {
             System.out.println("(1). Cadastrar Treinador");
             System.out.println("(2). Listar Treinadores");
             System.out.println("(3). Remover Treinador");
+            System.out.println("(4). Atualizar Treinador");
             System.out.println("(0). Sair");
             op = scan.nextInt();scan.nextLine();
             switch(op){
                 case 1 -> cadastrarT();
                 case 2 -> listarT();
                 case 3 -> removerT();
+                case 4 -> atualizarT();
                 case 0 -> System.out.println("Saindo...");
                 default -> System.out.println("Opção inválida!\nEscolha uma Opção Válida!!");
             }
@@ -30,10 +32,8 @@ public class TreinadorView {
     }
 
     public void cadastrarT(){
-        System.out.print("Nome: ");
-        String nome = scan.nextLine();
-        System.out.print("Idade: ");
-        int idade = scan.nextInt();scan.nextLine();
+        String nome = lerNome();
+        int idade = lerIdade();
         controleT.cadastrarTreinador(nome, idade);
     }
 
@@ -50,5 +50,69 @@ public class TreinadorView {
         controleT.removerTreinador(nome);
     }
 
+    public void atualizarT(){
+        System.out.print("Nome do treinador a atualizar: ");
+        String nomeAntigo = scan.nextLine();
+
+        System.out.println("Digite as novas informações: ");
+        String novoNome = lerNome();
+        int novaIdade = lerIdade();
+
+        controleT.atualizarTreinador(nomeAntigo, novoNome, novaIdade);
+    }
+
+    private String lerNome(){
+        String nome = "";
+        boolean valido = false;
+
+        while (!valido) {
+            try{
+                System.out.println("Digite o Nome com apenas Letras: ");
+                nome = scan.nextLine().trim();
+
+                if (nome.isEmpty()){
+                    throw new IllegalArgumentException("O Nome não pode estar vazio.");
+                }
+
+                for(char c : nome.toCharArray()){
+                    if (!Character.isLetter(c) && c != ' '){
+                        throw new IllegalArgumentException("O nome deve conter apenas letras e espaços.");
+                    }
+                }
+                valido = true;
+            } catch (IllegalArgumentException e){
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
+        return  nome;
+    }
+
+    private int lerIdade(){
+        int idade = -1;
+        boolean valido = false;
+
+        while(!valido){
+            try{
+                System.out.println("Digite a Idade entre (1 a 100): ");
+                String entrada = scan.nextLine().trim();
+
+                if (entrada.isEmpty()){
+                    throw new IllegalArgumentException("A idade não pode estar vazia.");
+                }
+                idade = Integer.parseInt(entrada);
+
+                if (idade < 1 || idade > 99){
+                    throw new IllegalArgumentException("A idade deve estar entre 1 e 99 anos");
+                }
+                valido = true;
+            }catch(NumberFormatException e){
+                System.out.println("Erro: Digite apenas números.");
+            }catch (IllegalArgumentException e ){
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
+
+        return idade;
+    }
 
 }
