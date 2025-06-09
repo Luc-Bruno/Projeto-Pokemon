902f902f5f3b69e958954a5ba27222141bf6c2bd
Swiatyi <Swiatyi44@gmail.com>
2025-06-09 17:18:35 -0300

    ArquivoTreinador
    Salva os Treinadores em arquivo .txt

--- src/persistencia/ArquivoTreinador.java
+++ src/persistencia/ArquivoTreinador.java
@@ -1,4 +1,46 @@
 package persistencia;
 
+import Model.Treinador;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class ArquivoTreinador {
+
+    private static final String CAMINHO =  "treinadores.txt";
+
+    public static void salvar(ArrayList<Treinador> treinadores){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(CAMINHO))){
+            for(Treinador t : treinadores){
+                bw.write(t.getNome() + ";" + t.getIdade());
+                bw.newLine();
+            }
+        }catch(IOException e){
+            System.out.println("Erro ao salvar Treinadores: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Treinador> carregar(){
+        ArrayList<Treinador> lista = new ArrayList<>();
+
+        File arquivoT = new File(CAMINHO);
+        if (!arquivoT.exists()){
+            return lista;
+        }
+        try(BufferedReader br = new BufferedReader(new FileReader(CAMINHO))){
+            String linha;
+            while((linha = br.readLine()) != null){
+                String[] partes  = linha.split(";");
+                if (partes.length ==2){
+                    String nome = partes[0];
+                    int idade = Integer.parseInt(partes[1]);
+                    lista.add(new Treinador(nome, idade));
+                }
+            }
+        }catch(IOException e){
+            System.out.println("Erro ao ler Treinadores: " + e.getMessage());
+        }
+        return lista;
+    }
+
 }
