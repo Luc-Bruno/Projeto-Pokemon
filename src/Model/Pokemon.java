e0fb58ba42bc57ef49240105d3a53d81f0a9b0e0
Felipe Furtuna <148819797+FelipeFurtuna@users.noreply.github.com>
2025-06-09 21:29:26 -0300

    classe abstrata Pokemon
    Cria a classe abstrata Pokemon com atributos básicos nome, nível, experiência e métodos comuns como getters, setters, subirNivel e ganharExperiencia. Serve como base para os tipos de Pokémon do sistema.

--- src/Model/Pokemon.java
+++ src/Model/Pokemon.java
@@ -1,18 +1,44 @@
 package Model;
 
-public abstract class Pokemon {
+public abstract class Pokemon implements Batalhavel {
 
-private String nome;
+protected String nome;
+protected int nivel;
+protected int experiencia;
 
-    public Pokemon(String nome) {
+    public Pokemon(String nome, int nivel) {
         this.nome = nome;
+        this.nivel = nivel;
+        this.experiencia = 0;
     }
+
+    public void setNivel(int nivel) {
+    this.nivel = nivel;
+}
 
     public String getNome() {
         return nome;
     }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    public int getNivel(){
+        return nivel;
     }
-}
+
+    public int getExperiencia(){
+        return experiencia;
+    }
+    public void ganharExperiencia(int xp){
+        this.experiencia+=xp;
+        if (experiencia >=100){
+            this.nivel++;
+            this.experiencia=0;
+            System.out.println(nome+" Subiu para o nivel: " +nivel+" !");
+        }
+    }
+@Override
+public abstract void atacar(Batalhavel oponente);
+
+
+    }
+
+
+
