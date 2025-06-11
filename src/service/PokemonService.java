1c7ff4c79347c4c50385b5f0f26aacee89acd0db
GiuseppeTNichele <giuseppetrinconichele@gmail.com>
2025-06-11 17:32:07 -0300

    Update PokemonService.java
    Implementação da classe PokemonService com métodos CRUD e integração com persistência de dados.

--- src/service/PokemonService.java
+++ src/service/PokemonService.java
@@ -1,4 +1,53 @@
 package service;
 
+import model.Pokemon;
+import persistencia.ArquivoPokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PokemonService {
+
+    private List<Pokemon> pokemons;
+    private ArquivoPokemon arquivo;
+
+    public PokemonService() {
+        this.arquivo = new ArquivoPokemon();
+        this.pokemons = arquivo.carregar();
+    }
+
+    public void adicionarPokemon(Pokemon p) {
+        pokemons.add(p);
+    }
+
+    public boolean removerPokemon(String nome) {
+        Pokemon encontrado = buscarPokemonPorNome(nome);
+        if (encontrado != null) {
+            pokemons.remove(encontrado);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Pokemon> listarPokemons() {
+        return new ArrayList<>(pokemons); // cópia da lista para segurança
+    }
+
+
+    public Pokemon buscarPokemonPorNome(String nome) {
+        for (Pokemon p : pokemons) {
+            if (p.getNome().equalsIgnoreCase(nome)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void salvarPokemons() {
+        arquivo.salvar(pokemons);
+    }
+
+    public void carregarPokemons() {
+        this.pokemons = arquivo.carregar();
+    }
 }
