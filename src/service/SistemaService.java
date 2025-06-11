cd57f748a2808ef9ad6e47c2ffed29a55369a51a
GiuseppeTNichele <giuseppetrinconichele@gmail.com>
2025-06-11 18:27:30 -0300

    Update SistemaService.java
    Implementação da classe SistemaService para coordenar carregamento e salvamento dos dados do sistema

--- src/service/SistemaService.java
+++ src/service/SistemaService.java
@@ -1,4 +1,38 @@
 package service;
 
 public class SistemaService {
+
+    private PokemonService pokemonService;
+    private TreinadorService treinadorService;
+    private BatalhaService batalhaService;
+
+    public SistemaService() {
+        this.pokemonService = new PokemonService();
+        this.treinadorService = new TreinadorService();
+        this.batalhaService = new BatalhaService();
+    }
+
+    public PokemonService getPokemonService() {
+        return pokemonService;
+    }
+
+    public TreinadorService getTreinadorService() {
+        return treinadorService;
+    }
+
+    public BatalhaService getBatalhaService() {
+        return batalhaService;
+    }
+
+    public void salvarTudo() {
+        pokemonService.salvarPokemons();
+        treinadorService.salvarTreinadores();
+        batalhaService.salvarBatalhas();
+    }
+    
+    public void carregarTudo() {
+        pokemonService.carregarPokemons();
+        treinadorService.carregarTreinadores();
+        batalhaService.carregarBatalhas();
+    }
 }
