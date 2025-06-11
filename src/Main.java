db28138104c36b44588eff4a6315d7c30f11585c
GiuseppeTNichele <giuseppetrinconichele@gmail.com>
2025-06-11 19:00:59 -0300

    Update Main.java
    Apenas um esqueleto da Main para editar depois

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
+import service.SistemaService;
+import view.MenuPrincipal;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        SistemaService sistema = new SistemaService();
 
-        }
+        sistema.carregarTudo();
+
+        MenuPrincipal menu = new MenuPrincipal(sistema);
+        menu.exibirMenu();
+
+        sistema.salvarTudo();
+        System.out.println("Dados salvos. Até mais!");
     }
+}
