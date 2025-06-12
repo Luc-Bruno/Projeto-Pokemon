3520ca43b0c53784dc9510775fd04e4317fcb6a3
Ângelo Panzera <angelopmata1@gmail.com>
2025-06-12 10:09:52 -0300

    Serviços Aux. e Console View.
    Criação dos serviços auxiliares e preenchimento do Console View.

--- src/Controller/ServicosAuxiliares.java
+++ src/Controller/ServicosAuxiliares.java
@@ -1,4 +1,16 @@
 package Controller;
 
 public class ServicosAuxiliares {
+
+    public static boolean nomeValido(String nome) {
+        return nome != null && nome.matches("[A-Za-zÀ-ÿ\\s]+");
+    }
+
+    public static boolean idadeValida(int idade) {
+        return idade > 0 && idade < 100;
+    }
+
+    public static boolean nivelValido(int nivel) {
+        return nivel >= 0 && nivel <= 100;
+    }
 }
--- src/View/ConsoleView.java
+++ src/View/ConsoleView.java
@@ -1,4 +1,44 @@
 package View;
 
+import java.util.List;
+import View.helpers.InputHelper;
+import Model.Treinador;
+import Model.Pokemon;
+import Model.Batalha;
+
 public class ConsoleView {
+    private InputHelper in = new InputHelper();
+
+    public void exibirMenuPrincipal() {
+        System.out.println("=== MENU PRINCIPAL ===");
+        System.out.println("1 - Gerenciar Treinadores");
+        System.out.println("2 - Gerenciar Pokémons");
+        System.out.println("3 - Gerenciar Batalhas");
+        System.out.println("0 - Sair");
+    }
+
+    public int lerOpcao() {
+        return in.lerInt("Escolha uma opção: ");
+    }
+
+    public void mostrarMensagem(String msg) {
+        System.out.println(msg);
+    }
+
+    public void mostrarErro(String erro) {
+        System.err.println("Erro: " + erro);
+    }
+
+    public void chamarMenuTreinador(Runnable menuTreinador) {
+        menuTreinador.run();
+    }
+
+    public void chamarMenuPokemon(Runnable menuPokemon) {
+        menuPokemon.run();
+    }
+
+    public void chamarMenuBatalha(Runnable menuBatalha) {
+        menuBatalha.run();
+    }
 }
+
